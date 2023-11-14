27bc6ab4a1836b54fc930c8e8afbb7d7ea1f3a43
Kirill Kliavin <k.kliavin@gmail.com>
2023-11-14 20:51:16 +1100

    functions

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,34 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+// import TheWelcome from '../components/TheWelcome.vue'
+
+import { ref } from 'vue'
+
+const isLoading = ref(true)
+const location = ref('')
+
+async function init(url) {
+  try {
+    isLoading.value = true
+    const response = await fetch(url)
+    const actualData = await response.data
+    location.value = actualData
+  } catch (e) {
+    console.error('Das ist der Catch Error: ', e)
+    location.value = 'Error'
+  } finally {
+    isLoading.value = false
+  }
+}
+
+init('/init')
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <!-- <TheWelcome /> -->
+    <div v-if="!isLoading">
+      {{ location }}
+    </div>
+    <div v-else>Loading...</div>
   </main>
 </template>
